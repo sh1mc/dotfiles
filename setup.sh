ed3cd5bbbc2fb23979d447ab1257f927e4ffd7e5
sh1mc <sh1@coins.tsukuba.ac.jp>
2020-11-06 05:51:35 +0900

    a

--- setup.sh
+++ setup.sh
@@ -2,5 +2,6 @@ DOT_FILES=(.bash_history .bash_profile .config .gitconfig .vim .bash_logout .bas
 
 for file in ${DOT_FILES[@]};
 do
+	rm -rf $HOME/$file
 	ln -s $HOME/dotfiles/$file $HOME/$file
 done
