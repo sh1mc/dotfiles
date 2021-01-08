3a9453f44f805555f2d8061302831c5ce549c968
sh1mc <sh1@coins.tsukuba.ac.jp>
2021-01-08 16:25:28 +0900

    fix func

--- .config/fish/functions/bvim.fish
+++ .config/fish/functions/bvim.fish
@@ -1,4 +1,4 @@
 # Defined in - @ line 1
-function bvim --wraps='nvim +:Vinarise' --description 'alias bvim=nvim +:Vinarise'
+function bvim --wraps='nvim +:Vinarise +":set modifiable" +":set write"' --description 'alias bvim=nvim +:Vinarise'
   nvim +:Vinarise $argv;
 end
